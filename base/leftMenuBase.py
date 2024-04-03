88069e3561fc2bb19ee4281973f641247ba2d061
yzn <597668385@qq.com>
2024-04-03 18:02:42 +0800

    左侧菜单栏定位

---
+++ base/leftMenuBase.py
@@ -0,0 +1,17 @@
+class LeftMenuBase:
+
+    def level_one_menu(self, menu_name):
+        """
+        一级菜单
+        :param menu_name:菜单栏名称
+        :return:
+        """
+        return "//aside[@class='el-aside']//span[text()='" + menu_name + "']/ancestor::li"
+
+    def level_two_menu(self, menu_name):
+        """
+        二级菜单
+        :param menu_name:菜单栏名称
+        :return:
+        """
+        return "//aside[@class='el-aside']//span[text()='" + menu_name + "']/ancestor::li[1]"
