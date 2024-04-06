fee6717f1fb7d8a35247021a34cdce4c47e113c2
yzn <597668385@qq.com>
2024-04-06 19:48:44 +0800

    ActionChains方法封装鼠标键盘操作

--- page/ActionChainPage.py
+++ page/ActionChainPage.py
@@ -6,11 +6,21 @@ from base.ActionChainsBase import ActionChainsBase
 
 class ActionChainPage(ObjectMap, ActionChainsBase):
     def click_exit_button(self, driver):
+        """
+        鼠标单击退出登录
+        :param driver:
+        :return:
+        """
         button_xpath = self.click_exit()
         action_chain = self.element_get(driver,locate_type=By.XPATH,locator_expression=button_xpath)
         return self.ActionChains_click(driver=driver, action_chains=action_chain)
 
     def mouse_move_to_list(self, driver):
+        """
+        鼠标移到右上角的头像
+        :param driver:
+        :return:
+        """
         _xpath = self.move_end()
         action_chain = self.element_get(driver=driver,locate_type=By.XPATH,locator_expression=_xpath)
         return self.ActionChains_move(driver=driver, action_chains=action_chain)
