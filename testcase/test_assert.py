f535301628637679e7690a77c57c771624accf9d
yzn <597668385@qq.com>
2024-04-07 13:38:38 +0800

    pytest_assume

--- testcase/test_assert.py
+++ testcase/test_assert.py
@@ -1,17 +1,27 @@
+import pytest
+from pytest_assume.plugin import assume
+
 class TestAssert:
     def test_assert(self):
-        # ==,!=,<,>,<=,>=
-        assert "dd" == "dd"
-        assert "aa" != "bb"
-        assert 0 < 1
-        assert 2 > 1
-        assert 3 <= 7 - 2
-        assert 4 >= 1 + 2
-        # 包含和不包含
-        assert "测开" in "从测试到测开"
-        assert "java" not in "从测试到测开"
-        # true和false
-        assert 1
-        assert (9<10) is True
-        assert not False
-        # 判断元素是否存在，在元素定位的时候，返回true和false，就直接断言了，不需要再去定义一个变量
+        # # ==,!=,<,>,<=,>=
+        # assert "dd" == "dd"
+        # assert "aa" != "bb"
+        # assert 0 < 1
+        # assert 2 > 1
+        # assert 3 <= 7 - 2
+        # assert 4 >= 1 + 2
+        # # 包含和不包含
+        # assert "测开" in "从测试到测开"
+        # assert "java" not in "从测试到测开"
+        # # true和false
+        # assert 1
+        # assert (9<10) is True
+        # assert not False
+        # # 判断元素是否存在，在元素定位的时候，返回true和false，就直接断言了，不需要再去定义一个变量
+
+        with assume: assert "william" in "UI autotest"
+
+        pytest.assume(1+1==3)
+        assert 1+1==2
+        print("over")
+
